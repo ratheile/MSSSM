e7de66f8a6b288e5319896fcaf1e491309a40656
sutery <sutery@student.ethz.ch>
2012-11-18 17:49:42 +0100

    Logikfunktionen sind soweit fertig

--- code/Iteration.m
+++ code/Iteration.m
@@ -1,9 +1,44 @@
-function [] = Iteration( agentsArray, wallArray, priorityArray )
+function [] = Iteration( agentsArray, wallArray )
 %	Funktion ruft die Funktion logicFunction auf
 %   Muss mit einer Prioritätenliste auf alle Agents ausgeweitet werden
 
-    angle = logicFunction(agentsArray, 1, 5, priorityArray);
+    global INFLUENCESPHERE PRECISIONCOLLISION DELTAT
+    
+    dist = linspace(0,1,PRECISIONCOLLISION);
+    
+    prioArray = getPriorityArray(agentsArray);
+    sortedPrioArray = getSortedPriorityArray(prioArray);    
+    
+    for k = sortedPrioArray
+        angleShift = logicFunction(agentsArray, k, INFLUENCESPHERE, prioArray, wallArray);
+        
+        %Kollisionstest        
+        %yCordOld = agentsArray(k).cordY;
+        xCordNeu = agentsArray(k).cordX + sin(angleShift) * DELTAT * agentsArray(k).maxSpeed * dist;
+        yCordNeu = agentsArray(k).cordY + cos(angleShift) * DELTAT * agentsArray(k).maxSpeed * dist;
+        distMat = zeros(length(sortedPrioArray)-1+length(wallArray),PRECISIONCOLLISION+1);
+        for l = sortedPrioArray
+            if l == k
+                continue
+            end
+            distMat(l,:) = [(sqrt((xCordNeu - agentsArray(l).cordX)^2 + (yCordNeu - agentsArray(l).cordY)^2) - agentsArray(k).radius - agentsArray(l).radius),-1]; %-1 als Sentinel                      
+        end
+        for l = 1:length(wallArray)
+            distMat(l,:) = [(sqrt((xCordNeu - wallArray(l).cordX)^2 + (yCordNeu - wallArray(l).cordY)^2) - agentsArray(k).radius - wallArray(l).radius),-1]; %-1 als Sentinel                                  
+        end
+        distMat = sort(distMat);
+        
+        maxL = find(distMat(1,:) < 0, 1) - 1;
+        if maxL == 0% Sollte eigentlich nicht geschehen
+            maxL = 1;
+        end
+        agentsArray(k).cordX = xCordNeu(maxL);
+        agentsArray(k).cordY = yCordNeu(maxL);
+        agentsArray(k).actSpeed = agentsArray(k).maxSpeed() * (maxL - 1) / (PRECISIONCOLLISION - 1);
+        
+    end    
 
 
 end
 
+     
--- code/defineConstants.m
+++ code/defineConstants.m
@@ -3,7 +3,7 @@ function [] = defineConstants()
 %   Detailed explanation goes here
 %Definition von Konstanten
 
-    global ANGLE GAUSSANGLE XSCOPE XRES XVALUES DELTAT HEIGHT SLOPEFACTOR WALLFACTOR AGENTANGLEOFFSET WALLANGLEOFFSET
+    global ANGLE GAUSSANGLE XSCOPE XRES XVALUES DELTAT HEIGHT SLOPEFACTOR WALLFACTOR AGENTANGLEOFFSET WALLANGLEOFFSET INFLUENCESPHERE PRECISIONCOLLISION
     XSCOPE = 20;
     XRES = 0.001;%Definiert die möglichen Winkel für die Logikfunktion
     XVALUES = -XSCOPE:XRES:XSCOPE;
@@ -16,7 +16,8 @@ function [] = defineConstants()
     WALLFACTOR = -5; %Faktor in der Stärke der Berechnung der Wand
     AGENTANGLEOFFSET = pi/18; %Offsetwinkel bei der Winkelberechnung für Agents
     WALLANGLEOFFSET = pi/18; %Offsetwinkel bei der Winkelberechung für Wand
-    
+    INFLUENCESPHERE = 5;
+    PRECISIONCOLLISION = 31;
     
 end
 
