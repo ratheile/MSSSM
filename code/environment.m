7a8344f1117979f21a14b071ea8a1acbfff03689
ratheile <rptheiler@gmail.com>
2012-11-18 11:30:25 +0100

    drawing and environment added

---
+++ code/environment.m
@@ -0,0 +1,14 @@
+classdef environment
+    %ENVIRONMENT Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties(
+        sizeX
+        sizeY
+    end
+    
+    methods
+    end
+    
+end
+
