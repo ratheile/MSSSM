c5b9c1f44d910a6e9338d527b6d6c5f519bee573
sutery <sutery@student.ethz.ch>
2012-11-28 21:04:59 +0100

    Angepasst

--- code/defineConstants.m
+++ code/defineConstants.m
@@ -1,24 +1,26 @@
 function [] = defineConstants()
 %defineConstants defines all global constants that will be used later
 
-    global ANGLE GAUSSANGLE XSCOPE XRES XVALUES DELTAT HEIGHT SLOPEFACTOR WALLFACTOR AGENTANGLEOFFSET WALLANGLEOFFSET INFLUENCESPHERE PRECISIONCOLLISION
+    global ANGLE GAUSSANGLE XSCOPE XRES XVALUES DELTAT HEIGHT SLOPEFACTOR WALLFACTOR AGENTANGLEOFFSET WALLANGLEOFFSET INFLUENCESPHERE PRECISIONCOLLISION SPEED
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %   Name & Value......................value.Description........................
-    XSCOPE = 20;                    % 20    how far one looks to the sides
-    XRES = 0.001;                   % 0.001 x-resolution, possible angles for logical functions
+    XSCOPE = 100;                    % 20    how far one looks to the sides
+    XRES = 0.005;                   % 0.001 x-resolution, possible angles for logical functions
     XVALUES = -XSCOPE:XRES:XSCOPE;  % ntbc* generate all possible x-values
     breite = 1;                     % 1     standard deviation of Gaussian function
                                     % determines the tendency to walk straightforwards
     GAUSSANGLE = gaussmf(XVALUES,[breite 0]); % ntbc* generate Gaussian function for direction
     ANGLE = atan(XVALUES);          % ntbc* calculate angular values for Gaussian function
-    DELTAT = 0.1;                   % 0.1   iteration steps in seconds
-    HEIGHT = 0.8;                   % 0.4   normalization factor in logical calculations for agents
-    SLOPEFACTOR = 10;                % 8     parameter for other agents' influence
-    WALLFACTOR = -0.1;                % -5    parameter for walls' influence
+    DELTAT = 0.03;                   % 0.1   iteration steps in seconds
+    HEIGHT = 3;                   % 0.4   normalization factor in logical calculations for agents
+    SLOPEFACTOR = 12;                % 8     parameter for other agents' influence
+    WALLFACTOR = -0.01;                % -5    parameter for walls' influence
     AGENTANGLEOFFSET = pi/12;       % pi/18 offset angle in angle calculations for agents
-    WALLANGLEOFFSET = pi/18;        % pi/18 offset angle in angle calculations for walls
-    INFLUENCESPHERE = 3;            % 5     how far an agents "looks", in meters
-    PRECISIONCOLLISION = 31;        % 31    numerical precision for collision detection
+    WALLANGLEOFFSET = pi/20;        % pi/18 offset angle in angle calculations for walls
+    INFLUENCESPHERE = 2;            % 5     how far an agents "looks", in meters
+    PRECISIONCOLLISION = 4;        % 31    numerical precision for collision detection
+    
+    SPEED = 0.01; %Wartezeit in Sekunden für das Abspielen zweier Schritte
     % *: ntbc = not to be changed!
 end
 
--- code/xValuesLogic.m
+++ code/xValuesLogic.m
@@ -15,14 +15,14 @@ function [ xOut ] = xValuesLogic( indexX, distance, betaLeft, betaRight, diffVel
         xOut = (xOut - min(xOut));
         xOut(indLeft:indRight) = 0;
         
-        xOut = xOut * HEIGHT / max(xOut) * radiusSum / distance;        
+        xOut = xOut * HEIGHT / max(xOut) * (radiusSum / distance)^2;        
         
         xOut(indLeftS:indLeft) = linspace(xOut(indLeftS),0,(indLeft-indLeftS+1));
         xOut(indRight:indRightS) = linspace(0,xOut(indRightS),(indRightS-indRight+1));
 
     elseif diffVelocity > 0
         alphaX = XVALUES(indexX);
-        xOut = gaussmf(XVALUES, [radiusSum/distance alphaX]) * diffVelocity * HEIGHT * radiusSum/distance;
+        xOut = gaussmf(XVALUES, [radiusSum/distance alphaX]) * diffVelocity * HEIGHT/5 * radiusSum/distance;
         
     else
         xOut = zeros(1,length(XVALUES));
