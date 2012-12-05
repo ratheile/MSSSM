1948ed64f6033d8cd5fd6cc031567777f30d6ee7
sutery <sutery@student.ethz.ch>
2012-12-05 22:37:43 +0100

    Simplified

--- code/getBeta.m
+++ code/getBeta.m
@@ -2,7 +2,7 @@ function [betaLeft, betaRight] = getBeta(radiusSum, alpha, distance)
 %getBeta: For two agents: for given radii, angle alpha (direction of other agent wrt. x-Axis) and the distance between, this function calculates the angles outside. See documentation for the derivation and exact meaning of alpha, betaLeft, betaRight and gamma
     
     gamma = acos(radiusSum / distance);
-    betaRight = pi/2 - (gamma + alpha - pi/2);
-    betaLeft = pi/2 - (pi/2 + alpha - gamma);
+    betaRight = pi - (gamma + alpha);   %betaRight = pi/2 - (gamma + alpha - pi/2);
+    betaLeft = - alpha + gamma;         %betaLeft = pi/2 - (pi/2 + alpha - gamma);
 
 end
