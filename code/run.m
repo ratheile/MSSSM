0202dbb5cf4e4bbe6da229b86764775e95259b92
sutery <sutery@student.ethz.ch>
2012-12-09 16:24:08 +0100

    Mehrere Matlabinstanzen gleichzeitig laufen lassen moeglich

--- code/run.m
+++ code/run.m
@@ -10,16 +10,18 @@ sim = simulation();
 sim.init()
 
 mode = input('Which runmode? ','s');
-sim.runMode(mode);
-
 ifSave = input('Save output [Y/N]? ','s');
-
 if ifSave == 'Y'
     outName = input('Write data to: ', 's');
     copyfile('defineConstants.m',['sim/' outName '.txt']);
     fileID = fopen(['sim/' outName '.txt'],'a+');
     fprintf(fileID,['\nMode utilized: ' mode]);
     fclose(fileID);
+end
+
+sim.runMode(mode);
+
+if ifSave == 'Y'
     save(['sim/' outName '.mat']);
     print('-dpng',['sim/' outName '.png']);
     %load(<pfad>)
