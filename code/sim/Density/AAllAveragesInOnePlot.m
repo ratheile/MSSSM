65a460fd5f47ff249fe8e998b3734169b9d469bd
moserman <manuel.m@gmx.ch>
2012-12-13 23:47:54 +0100

    new colour to avoid doucle use of blue

--- code/sim/Density/AAllAveragesInOnePlot.m
+++ code/sim/Density/AAllAveragesInOnePlot.m
@@ -2,11 +2,11 @@
 %different densities against time.
 
 hold on;
-plot(0.1:0.1:120, AgentsInSystem1up1downAvg, 0.1:0.1:120, AgentsInSystem1up08downAvg, 0.1:0.1:120, AgentsInSystem1up06downAvg, 0.1:0.1:120, AgentsInSystem1up04downAvg, 0.1:0.1:120, AgentsInSystem08up08downAvg, 0.1:0.1:120, AgentsInSystem08up06downAvg, 0.1:0.1:120, AgentsInSystem08up04downAvg, 0.1:0.1:120, AgentsInSystem06up06downAvg);
+plot(0.1:0.1:120, AgentsInSystem1up1downAvg, 0.1:0.1:120, AgentsInSystem1up08downAvg, 0.1:0.1:120, AgentsInSystem1up06downAvg, 0.1:0.1:120, AgentsInSystem1up04downAvg, 0.1:0.1:120, AgentsInSystem08up08downAvg, 0.1:0.1:120, AgentsInSystem08up06downAvg, 0.1:0.1:120, AgentsInSystem08up04downAvg);
+plot(0.1:0.1:120, AgentsInSystem06up06downAvg, 'Color', [1 0 1]);
 plot(0.1:0.1:120, AgentsInSystem06up04downAvg, 'Color', [1 153/253 51/253]);
 plot(0.1:0.1:120, AgentsInSystem04up04downAvg, 'Color', [153/255 1 0]);
 legend('1 up, 1 down','1 up, 0.8 down','1 up, 0.6 down','1 up, 0.4 down','0.8 up, 0.8 down','0.8 up, 0.6 down','0.8 up, 0.4 down','0.6 up, 0.6 down','0.6 up, 0.4 down','0.4 up, 0.4 down','Location','NorthWest');
-hold off;
-
 xlabel('Time in Seconds'); ylabel('Number of Agents in System');
 title('Number of agents compared with various densities');
+hold off;
--- code/sim/Density/AAllAveragesScaled.m
+++ code/sim/Density/AAllAveragesScaled.m
@@ -2,7 +2,8 @@
 %so it's a scaled version of the one above
 
 hold on;
-plot(0.1:0.1:120, AIS1up1downAvgScaled, 0.1:0.1:120, AIS1up08downAvgScaled, 0.1:0.1:120, AIS1up06downAvgScaled, 0.1:0.1:120, AIS1up04downAvgScaled, 0.1:0.1:120, AIS08up08downAvgScaled, 0.1:0.1:120, AIS08up06downAvgScaled, 0.1:0.1:120, AIS08up04downAvgScaled, 0.1:0.1:120, AIS06up06downAvgScaled);
+plot(0.1:0.1:120, AIS1up1downAvgScaled, 0.1:0.1:120, AIS1up08downAvgScaled, 0.1:0.1:120, AIS1up06downAvgScaled, 0.1:0.1:120, AIS1up04downAvgScaled, 0.1:0.1:120, AIS08up08downAvgScaled, 0.1:0.1:120, AIS08up06downAvgScaled, 0.1:0.1:120, AIS08up04downAvgScaled);
+plot(0.1:0.1:120, AIS06up06downAvgScaled, 'Color', [1 0 1]);
 plot(0.1:0.1:120, AIS06up04downAvgScaled, 'Color', [1 153/253 51/253]);
 plot(0.1:0.1:120, AIS04up04downAvgScaled, 'Color', [153/255 1 0]);
 legend('1 up, 1 down','1 up, 0.8 down','1 up, 0.6 down','1 up, 0.4 down','0.8 up, 0.8 down','0.8 up, 0.6 down','0.8 up, 0.4 down','0.6 up, 0.6 down','0.6 up, 0.4 down','0.4 up, 0.4 down','Location','NorthWest');
