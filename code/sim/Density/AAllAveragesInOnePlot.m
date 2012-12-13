1e0041778656a278be1ed1733bd6a0e3d0998fa0
moserman <manuel.m@gmx.ch>
2012-12-13 23:14:12 +0100

    Density plots renewed

---
+++ code/sim/Density/AAllAveragesInOnePlot.m
@@ -0,0 +1,12 @@
+%This one plots the average number of agents (of the three seeds) in the
+%different densities against time.
+
+hold on;
+plot(0.1:0.1:120, AgentsInSystem1up1downAvg, 0.1:0.1:120, AgentsInSystem1up08downAvg, 0.1:0.1:120, AgentsInSystem1up06downAvg, 0.1:0.1:120, AgentsInSystem1up04downAvg, 0.1:0.1:120, AgentsInSystem08up08downAvg, 0.1:0.1:120, AgentsInSystem08up06downAvg, 0.1:0.1:120, AgentsInSystem08up04downAvg, 0.1:0.1:120, AgentsInSystem06up06downAvg);
+plot(0.1:0.1:120, AgentsInSystem06up04downAvg, 'Color', [1 153/253 51/253]);
+plot(0.1:0.1:120, AgentsInSystem04up04downAvg, 'Color', [153/255 1 0]);
+legend('1 up, 1 down','1 up, 0.8 down','1 up, 0.6 down','1 up, 0.4 down','0.8 up, 0.8 down','0.8 up, 0.6 down','0.8 up, 0.4 down','0.6 up, 0.6 down','0.6 up, 0.4 down','0.4 up, 0.4 down','Location','NorthWest');
+hold off;
+
+xlabel('Time in Seconds'); ylabel('Number of Agents in System');
+title('Number of agents compared with various densities');
